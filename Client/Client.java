f86e0f26599bf48f3a5326397fb9e9a5605e780b
Tyler Tan <tylerjhtan888@gmail.com>
2023-12-04 13:50:01 +0800

    Fix: Cannot make ip empty, Made GUI flow better

--- Client/Client.java
+++ Client/Client.java
@@ -19,7 +19,7 @@ public class Client {
     public Client() {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(360, 720);
+        frame.setSize(400, 820);
         frame.setResizable(false);
         frame.setTitle("チットチャット Client");
         frame.setLocationRelativeTo(null);
@@ -27,118 +27,156 @@ public class Client {
 
         Font font = new Font("Comic Sans", Font.BOLD, 14);
 
+        JPanel joinPanel = new JPanel(new FlowLayout());
+
         JLabel joinLabel = new JLabel("/join");
         joinLabel.setPreferredSize(new Dimension(30, 30));
         joinLabel.setFont(font);
-        frame.add(joinLabel);
+        joinPanel.add(joinLabel);
 
         JTextField joinFieldIP = new JTextField("", 10);
         joinFieldIP.setPreferredSize(new Dimension(100, 30));
-        frame.add(joinFieldIP);
+        joinPanel.add(joinFieldIP);
 
         JTextField joinFieldPort = new JTextField("", 5);
         joinFieldPort.setPreferredSize(new Dimension(50, 30));
-        frame.add(joinFieldPort);
+        joinPanel.add(joinFieldPort);
 
         JButton joinButton = new JButton("Connect");
         joinButton.setPreferredSize(new Dimension(100, 30));
-        frame.add(joinButton);
+        joinPanel.add(joinButton);
+
+        frame.add(joinPanel);
+
+        JPanel leavePanel = new JPanel(new FlowLayout());
 
         JLabel leaveLabel = new JLabel("/leave");
         leaveLabel.setFont(font);
         leaveLabel.setPreferredSize(new Dimension(50, 30));
-        frame.add(leaveLabel);
+        leavePanel.add(leaveLabel);
 
         JButton leaveButton = new JButton("Disconnect");
         leaveButton.setPreferredSize(new Dimension(230, 30));
-        frame.add(leaveButton);
+        leavePanel.add(leaveButton);
+
+        frame.add(leavePanel);
+
+        JPanel registerPanel = new JPanel(new FlowLayout());
 
         JLabel registerLabel = new JLabel("/register");
         registerLabel.setFont(font);
         registerLabel.setPreferredSize(new Dimension(70, 30));
-        frame.add(registerLabel);
+        registerPanel.add(registerLabel);
 
         JTextField registerField = new JTextField("", 14);
         registerField.setPreferredSize(new Dimension(150, 30));
-        frame.add(registerField);
+        registerPanel.add(registerField);
 
         JButton registerButton = new JButton("Register");
         registerButton.setPreferredSize(new Dimension(100, 30));
-        frame.add(registerButton);
+        registerPanel.add(registerButton);
+
+        frame.add(registerPanel);
+
+        JPanel storePanel = new JPanel(new FlowLayout());
 
         JLabel storeLabel = new JLabel("/store");
         storeLabel.setFont(font);
         storeLabel.setPreferredSize(new Dimension(50, 30));
-        frame.add(storeLabel);
+        storePanel.add(storeLabel);
 
         JTextField storeField = new JTextField("", 16);
         storeField.setPreferredSize(new Dimension(100, 30));
-        frame.add(storeField);
+        storePanel.add(storeField);
 
         JButton storeButton = new JButton("Store");
         storeButton.setPreferredSize(new Dimension(100, 30));
-        frame.add(storeButton);
+        storePanel.add(storeButton);
+
+        frame.add(storePanel);
+
+        JPanel dirPanel = new JPanel(new FlowLayout());
 
         JLabel dirLabel = new JLabel("/dir");
         dirLabel.setFont(font);
         dirLabel.setPreferredSize(new Dimension(30, 30));
-        frame.add(dirLabel);
+        dirPanel.add(dirLabel);
 
         JButton dirButton = new JButton("List Files");
         dirButton.setPreferredSize(new Dimension(280, 30));
-        frame.add(dirButton);
+        dirPanel.add(dirButton);
+
+        frame.add(dirPanel);
+
+        JPanel getPanel = new JPanel(new FlowLayout());
 
         JLabel getLabel = new JLabel("/get");
         getLabel.setFont(font);
         getLabel.setPreferredSize(new Dimension(50, 30));
-        frame.add(getLabel);
+        getPanel.add(getLabel);
 
         JTextField getField = new JTextField("", 17);
         getField.setPreferredSize(new Dimension(100, 30));
-        frame.add(getField);
+        getPanel.add(getField);
 
         JButton getButton = new JButton("Retrieve");
         getButton.setPreferredSize(new Dimension(100, 30));
-        frame.add(getButton);
+        getPanel.add(getButton);
+
+        frame.add(getPanel);
+
+        JPanel msgPanel = new JPanel(new FlowLayout());
 
         JLabel msgLabel = new JLabel("/msg");
         msgLabel.setFont(font);
         msgLabel.setPreferredSize(new Dimension(40, 30));
-        frame.add(msgLabel);
+        msgPanel.add(msgLabel);
 
         JTextField msgField1 = new JTextField("", 5);
         msgField1.setPreferredSize(new Dimension(100, 30));
-        frame.add(msgField1);
+        msgPanel.add(msgField1);
 
         JTextField msgField2 = new JTextField("", 14);
         msgField2.setPreferredSize(new Dimension(100, 30));
-        frame.add(msgField2);
+        msgPanel.add(msgField2);
 
         JButton msgButton = new JButton("Send");
         msgButton.setPreferredSize(new Dimension(70, 30));
-        frame.add(msgButton);
+        msgPanel.add(msgButton);
+
+        frame.add(msgPanel);
+
+        JPanel allPanel = new JPanel(new FlowLayout());
 
         JLabel allLabel = new JLabel("/all");
         allLabel.setFont(font);
         allLabel.setPreferredSize(new Dimension(30, 30));
-        frame.add(allLabel);
+        allPanel.add(allLabel);
 
         JTextField allField = new JTextField("", 15);
         allField.setPreferredSize(new Dimension(30, 30));
-        frame.add(allField);
+        allPanel.add(allField);
 
         JButton allButton = new JButton("Broadcast");
         allButton.setPreferredSize(new Dimension(100, 30));
-        frame.add(allButton);
+        allPanel.add(allButton);
+
+        frame.add(allPanel);
+
+        JPanel helpPanel = new JPanel(new FlowLayout());
 
         JLabel helpLabel = new JLabel("/?");
         helpLabel.setFont(font);
         helpLabel.setPreferredSize(new Dimension(50, 30));
-        frame.add(helpLabel);
+        helpPanel.add(helpLabel);
 
         JButton helpButton = new JButton("Need help with commands?");
         helpButton.setPreferredSize(new Dimension(230, 30));
-        frame.add(helpButton);
+        helpPanel.add(helpButton);
+
+        frame.add(helpPanel);
+
+        JPanel displayPanel = new JPanel(new FlowLayout());
 
         JTextArea displayArea = new JTextArea();
         displayArea.setEditable(false);
@@ -147,9 +185,11 @@ public class Client {
 
         JScrollPane scrollArea = new JScrollPane(displayArea,
                 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollArea.setPreferredSize(new Dimension(300, 340));
-        
-        frame.add(scrollArea);
+        scrollArea.setPreferredSize(new Dimension(340, 300));
+
+        displayPanel.add(scrollArea);
+
+        frame.add(displayPanel);
 
         joinButton.addActionListener(new ActionListener() {
             @Override
@@ -162,6 +202,12 @@ public class Client {
                 try {
                     String ip = joinFieldIP.getText().trim();
                     int port = Integer.parseInt(joinFieldPort.getText().trim());
+                    if (ip.equals("")) {
+                        displayArea
+                                .append("Error: Connection to the Server has failed! IP Address cannot be empty! \n\n");
+                        return;
+                    }
+
                     clientEndpoint = new Socket();
                     clientEndpoint.connect(new InetSocketAddress(ip, port), 1000);
                     writer = new DataOutputStream(clientEndpoint.getOutputStream());
@@ -206,7 +252,7 @@ public class Client {
                 }
                 try {
                     String handle = registerField.getText().trim();
-                    if(handle.equals("")){
+                    if (handle.equals("")) {
                         displayArea.append("Error: Registration failed. Handle cannot be empty.\n\n");
                         return;
                     }
@@ -234,7 +280,7 @@ public class Client {
                         displayArea.append("Error: Storage failed. Filename is not allowed.\n\n");
                         return;
                     }
-                    if(filename.equals("")){
+                    if (filename.equals("")) {
                         displayArea.append("Error: Storage failed. Filename cannot be empty.\n\n");
                         return;
                     }
@@ -291,7 +337,7 @@ public class Client {
                 }
                 try {
                     String filename = getField.getText().trim();
-                    if(filename.equals("")){
+                    if (filename.equals("")) {
                         displayArea.append("Error: File retrieval failed. Filename cannot be empty.\n\n");
                         return;
                     }
@@ -326,12 +372,12 @@ public class Client {
                 }
                 try {
                     String handle = msgField1.getText().trim();
-                    if(handle.equals("")){
+                    if (handle.equals("")) {
                         displayArea.append("Error: Message sending failed. Handle cannot be empty.\n\n");
                         return;
                     }
                     String message = msgField2.getText().trim();
-                    if(message.equals("")){
+                    if (message.equals("")) {
                         displayArea.append("Error: Message sending failed. Message cannot be empty.\n\n");
                         return;
                     }
@@ -348,12 +394,13 @@ public class Client {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (clientEndpoint == null) {
-                    displayArea.append("Error: Broadcast message sending failed. Please connect to the server first.\n\n");
+                    displayArea
+                            .append("Error: Broadcast message sending failed. Please connect to the server first.\n\n");
                     return;
                 }
                 try {
                     String message = allField.getText().trim();
-                    if(message.equals("")){
+                    if (message.equals("")) {
                         displayArea.append("Error: Broadcast message sending failed. Message cannot be empty.\n\n");
                         return;
                     }
@@ -384,7 +431,7 @@ public class Client {
 
         frame.setVisible(true);
 
-        while(true){
+        while (true) {
             try {
                 Thread.sleep(500);
             } catch (Exception e) {
